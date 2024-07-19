af66d16ab47e7aad86a6a3c259695981e9e30ff9
Grégory VORBE <fluidz91@yahoo.fr>
2024-07-19 02:47:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+findIndex = (a1, a2) => {
+    let startIndex = -1;
+    for (let i = 0; i < a1.length; i++) {
+      if (a1[i] === a2[0]) {
+        let found = true;
+        for (let j = 1; j < a2.length; j++) {
+          if (a1[i + j] !== a2[j]) {
+            found = false;
+            break;
+          }
+        }
+        if (found) {
+          startIndex = i;
+          break;
+        }
+      }
+    }
+    //console.log('index: ' + startIndex);
+    return startIndex;
+}
 let req = indexedDB.open("/idbfs", 21);
 req.onsuccess = (event)=>{
     let db = req.result; 
@@ -7,36 +27,54 @@ req.onsuccess = (event)=>{
         if(cursor){
             if (cursor.key.includes('PlayerPrefs')) {
                 let contents = cursor.value.contents;
+                let has_changed = false;
                 //console.log("==>"+contents);
-                let money_uint8 = new TextEncoder().encode("money");
+                const money_uint8 = new TextEncoder().encode("money");
                 //console.log("==>"+ money_uint8);
-                let startIndex = -1;
-                for (let i = 0; i < contents.length; i++) {
-                    if (contents[i] === money_uint8[0]) {
-                        let found = true;
-                        for (let j = 1; j < money_uint8.length; j++) {
-                            if (contents[i + j] !== money_uint8[j]) {
-                                found = false;
-                                break;
-                            }
-                        }
-                        if (found) {
-                            startIndex = i;
-                            break;
-                        }
-                    }
+                index = findIndex(contents, money_uint8);
+                if (index !== -1) {
+                    has_changed = true;
+                    contents[index+money_uint8.length+1] = 255;
+                    contents[index+money_uint8.length+2] = 255;
+                    contents[index+money_uint8.length+3] = 255;
                 }
-                if (startIndex !== -1) {
-                    contents[startIndex+money_uint8.length+1] = 255;
-                    contents[startIndex+money_uint8.length+2] = 255;
-                    contents[startIndex+money_uint8.length+3] = 255;
+
+                // Disabled: can cause a game crash 
+                /*const food1_uint8 = new TextEncoder().encode("foodAmount1");
+                index = findIndex(contents, food1_uint8);
+                if (index !== -1) {
+                    has_changed = true;
+                    contents[index+food1_uint8.length+1] = 255;
+                }
+
+                const food2_uint8 = new TextEncoder().encode("foodAmount2");
+                index = findIndex(contents, food2_uint8);
+                if (index !== -1) {
+                    has_changed = true;
+                    contents[index+food2_uint8.length+1] = 255;
+                }
+
+                const battery_uint8 = new TextEncoder().encode("battery");
+                index = findIndex(contents, battery_uint8);
+                if (index !== -1) {
+                    has_changed = true;
+                    contents[index+battery_uint8.length+1] = 10;
+                }
+
+                const exp_uint8 = new TextEncoder().encode("_exp");
+                index = findIndex(contents, exp_uint8);
+                if (index !== -1) {
+                    has_changed = true;
+                    contents[index+exp_uint8.length+1] = 10;
+                }*/
+
+                if(has_changed){
                     cursor.value.contents = contents;
                     let ru = os.put(cursor.value, cursor.key);
                     ru.onsuccess = () => {
                         console.log('save game updated!')
                     }
                 }
-                //return;
             }
             cursor.continue();
         }
